6b532e0ebd30a4dbadef434032e9fc45bd448395
Oleg Shuruev <shuruev@gmail.com>
2022-06-06 15:17:54 -0700

    Initial PathUtil implementation with a 'Scan' method.

--- Atom/Util/PathUtil/PathUtil.cs
+++ Atom/Util/PathUtil/PathUtil.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+#nullable enable
 
 namespace Atom.Util
 {
@@ -12,12 +11,46 @@ namespace Atom.Util
     /// </summary>
     public static class PathUtil
     {
-        /*public static void EnsureAbsolute(string path)
+        /// <summary>
+        /// Makes sure that specified path was absolute (i.e. non-relative),
+        /// and throws <see cref="InvalidOperationException"/> otherwise.
+        /// </summary>
+        public static void EnsureAbsolute(string path)
         {
             if (!Path.IsPathFullyQualified(path))
                 throw new InvalidOperationException($"Path should be absolute: {path}");
         }
 
+        /// <summary>
+        /// Scans specified path with various parameters. Can search for files and/or directories,
+        /// and use custom predicate functions for whether an item (file or directory) should be returned,
+        /// as well as whether the search should continue recursively.
+        /// </summary>
+        /// <param name="pathToScan">Base path to scan. All result items will get their relative paths based on this location.</param>
+        /// <param name="recursive">Whether scan should continue recursively, or be limited to the specified directory only</param>
+        /// <param name="includeFiles">Whether files should be included as result items</param>
+        /// <param name="includeDirectories">Whether directories should be included as result items</param>
+        /// <param name="matchPredicate">
+        /// A function determining whether an item (file or directory) should be included into result or ignored, e.g.
+        /// <code>
+        /// // find files by extension (case-insensitive)
+        /// matchPredicate: item =>
+        ///     String.Equals(Path.GetExtension(item.Name), ".txt", StringComparison.OrdinalIgnoreCase));
+        /// </code>
+        /// </param>
+        /// <param name="scanPredicate">
+        /// A function determining whether the search should continue for this directory, e.g.
+        /// <code>
+        /// // do not search in certain folders
+        /// scanPredicate: item =>
+        ///     item.Name != ".git"
+        ///     && item.Name != "bin"
+        ///     && item.Name != "obj"
+        ///     && item.Name != "node_modules");
+        /// </code>
+        /// Not used when <paramref name="recursive"/> is <c>false</c>.
+        /// </param>
+        /// <returns>A list of items (files and directories) that were found</returns>
         public static List<PathItem> Scan(
             string pathToScan,
             bool recursive = true,
@@ -92,14 +125,29 @@ namespace Atom.Util
                     }
                 }
             }
-        }*/
+        }
     }
 
+    /// <summary>
+    /// Holds file (or directory) position, relatively to some specific base path.
+    /// </summary>
     public class PathItem
     {
-        //public string Name { get; set; } = null!;
-        //public bool IsDirectory { get; set; }
-        //public string AbsolutePath { get; set; } = null!;
-        //public string RelativePath { get; set; } = null!;
+        /// <summary>
+        /// File (or directory name), e.g. "MyFile.txt".
+        /// </summary>
+        public string Name { get; set; } = null!;
+        /// <summary>
+        /// Indicates whether current item is a directory.
+        /// </summary>
+        public bool IsDirectory { get; set; }
+        /// <summary>
+        /// Absolute path, e.g. "C:\My\Files\MyFile.txt".
+        /// </summary>
+        public string AbsolutePath { get; set; } = null!;
+        /// <summary>
+        /// Relative path, e.g. "/Files/MyFile.txt".
+        /// </summary>
+        public string RelativePath { get; set; } = null!;
     }
 }
